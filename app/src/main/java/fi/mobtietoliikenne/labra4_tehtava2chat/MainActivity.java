74f06982a96381a7b11f21ff64a11865db32d55f
Niko Kontu <t7koni00@students.oamk.fi>
2019-12-04 14:01:57 +0200

    Initial commit

---
+++ app/src/main/java/fi/mobtietoliikenne/labra4_tehtava2chat/MainActivity.java
@@ -0,0 +1,14 @@
+package fi.mobtietoliikenne.labra4_tehtava2chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
